275c659cce9e4267adcb6ffc6e8bc1222c04505a
tatikaze <k017c1356@it-neec.jp>
2019-05-16 02:25:54 +0900

    [updata] 日記のデータを取得するapiを作成

--- app.js
+++ app.js
@@ -4,10 +4,33 @@ const app = express();  // エクスプレスのインスタンス生成
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // /にアクセスされたときの処理
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+app.get('/diarys', (req, res) => {
+    res.json(data);
+});
+
+// app.post('/post', (req, res) => {
+//     console.log(req);
+//     res.json();
+// });
+
+var data = {
+  diarys: [
+    {title: 'もうだめだ', content: 'おしまいだぁ'},
+    {title: 'Vtuberすこっていけ', content: 'バ美肉おじさんは救いです'},
+    {title: 'オーバーウォッチくそげー', content: '殺さないでください'}
+  ]
+}
 
 // 3000番で待受
 app.listen(3000, () => {

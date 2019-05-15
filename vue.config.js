180304a07473a261dce8acbf1a5d961d3b0a4a37
tatikaze <k017c1356@it-neec.jp>
2019-05-15 18:30:57 +0900

    [fix] ファイリングを変更

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    configureWebpack: {
+        resolve: {
+          alias: {
+            '@': path.join(__dirname, '/src/') // 1. @の参照先の変更
+          }
+        }
+    },
+    outputDir: 'public/', // 2. 出力先
+    pages: {
+        index: {
+            entry: 'src/main/client/main.ts', // エントリーポイント
+            template: 'dist/index.html', //3. index.htmlテンプレート
+            filename: 'index.html' // 省略可
+        }
+    }
+}
